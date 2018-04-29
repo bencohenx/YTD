3f23113e0dcc69fecf14561c340f75f52f4e5baf
Ben <Benc10128@gmail.com>
2018-04-29 16:28:44 -0700

    change script

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import requests
 from bs4 import BeautifulSoup as bs4
-import base64
 
 
 def save_html_youtube(html):
@@ -19,19 +18,25 @@ def save_html_mp3io(html):
 def get_url_vid(name):
     search_results = requests.get(youtube_results_url, params={'search_query': name})
     html_doc = search_results.content
-    save_html_youtube(html_doc)
+    # save_html_youtube(html_doc)
     soup = bs4(html_doc, 'lxml')
     vid_id = soup.findAll("button", {"title": "Watch later"})[0].get("data-video-ids")
     return 'https://www.youtube.com/watch?v=' + vid_id
 
 
-def download_mp3(yb_url):
+def get_download_url(yb_url):
     mp3_results = requests.get(mp3io_download_url, params={'video': yb_url})
     html_doc = mp3_results.content
-    save_html_mp3io(html_doc)
+    # save_html_mp3io(html_doc)
     soup = bs4(html_doc, 'lxml')
     href_id = soup.findAll('a', {"id": "download"})[0].get("href")
     return 'http://www.convertmp3.io' + href_id
+
+
+def save_file(name, d_url):
+    tmp = requests.get(d_url)
+    with open('Downloads/' + name + '.mp3', 'wb') as f:
+        f.write(tmp.content)
 
 
 songs_list = open('songs.txt', "r", encoding="utf8")
@@ -41,7 +46,9 @@ mp3io_download_url = 'http://www.convertmp3.io/download/'
 songs_dict = {}
 for song in songs_list.read().split('\n'):
     youtube_url = get_url_vid(song.replace(" ", "+"))
-    mp3io_to_download = download_mp3(youtube_url)
-    songs_dict[song] = [youtube_url,mp3io_to_download]
-
-print(songs_dict)
+    mp3io_to_download = get_download_url(youtube_url)
+    save_file(song, mp3io_to_download)
+    songs_dict[song] = [youtube_url, mp3io_to_download]
+    print("")
+    print("****************************")
+    print("finish with: "+song)
