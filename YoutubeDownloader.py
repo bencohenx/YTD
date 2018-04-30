6b32adb682ecc809143af96d335b7df0f4e9d18a
ben <benc10128@gmail.com>
2018-04-30 14:57:35 +0300

    changing argparse

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup as bs4
 import argparse
 import os.path
+import sys
 
 parser = argparse.ArgumentParser(description='''
 __   __             _           _                                
@@ -25,7 +26,8 @@ group = parser.add_mutually_exclusive_group()
 parser.add_argument('-s', "--song-name")
 parser.add_argument('-l', '--list-path')
 parser.add_argument('-o', '--output-path', default="YoutubeDownloaderAudio")
-group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
+
 
 def save_html_youtube(html):
     with open("youtuberesult.html", 'wb') as f:
@@ -58,16 +60,35 @@ def get_download_url(yb_url):
 
 
 def save_file(name, d_url):
-    tmp = requests.get(d_url)
     with open(os.path.join(args.output_path, name) + '.mp3', 'wb') as f:
-        f.write(tmp.content)
+        response = requests.get(d_url, stream=True)
+        if args.quiet:
+            f.write(response.content)
+        else:
+            total_length = response.headers.get('content-length')
+            if total_length is None:  # no content length header
+                f.write(response.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in response.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
+                    done = int(50 * dl / total_length)
+                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                    sys.stdout.flush()
+                print('\n')
 
 
-def final(song,download_path):
-    youtube_url = get_url_vid(song.replace(" ", "+"))
+def final(song_name, song_id_list, list_amount):
+    if args.quiet:
+        print('Download: '+song_name)
+    else:
+        print("Start download "+str(song_id_list)+"/"+str(list_amount)+": " + song_name)
+    youtube_url = get_url_vid(song_name.replace(" ", "+"))
     mp3io_to_download = get_download_url(youtube_url)
-    save_file(song, mp3io_to_download)
-    songs_dict[song] = [youtube_url, mp3io_to_download]
+    save_file(song_name, mp3io_to_download)
+    songs_dict[song_name] = [youtube_url, mp3io_to_download]
 
 
 youtube_results_url = "https://www.youtube.com/results"
@@ -75,6 +96,7 @@ mp3io_download_url = 'http://www.convertmp3.io/download/'
 songs_dict = {}
 args = parser.parse_args()
 
+print("Running '{}'\n".format(__file__))
 
 if args.output_path is not None:
     if os.path.isdir(args.output_path):
@@ -83,23 +105,23 @@ if args.output_path is not None:
         os.makedirs(args.output_path)
 
 if args.song_name is not None:
-    final(args.song_name, args.output_path)
+    final(args.song_name, '1', '1')
 elif args.list_path is not None:
-    if os.path.exists(args.list_path):
-        songs_list = open(args.list_path, "r", encoding="utf8")
-        for song in songs_list.read().split('\n'):
-            final(song, args.output_path)
+    if os.path.exists(args.list_path):  # works on songs list
+        with open(args.list_path, "r", encoding="utf8") as f:
+            songs_list = f.read().split('\n')
+        i = 1
+        amount = len(songs_list)
+        for song in songs_list:
+            final(song, i, amount)
+            i = i + 1
     else:
-        print('list is not exists')
-
-'''
-songs_list = open('songs.txt', "r", encoding="utf8")
-for song in songs_list.read().split('\n'):
-    youtube_url = get_url_vid(song.replace(" ", "+"))
-    mp3io_to_download = get_download_url(youtube_url)
-    save_file(song, mp3io_to_download)
-    songs_dict[song] = [youtube_url, mp3io_to_download]
-    print("")
-    print("****************************")
-    print("finish with: "+song)
-'''
+        print('''
+              *** Wrong path list.. ***
+              ''')
+else:
+    print('''
+    *****************************************
+    ***  Bad Args, try the help, --help   ***
+    *****************************************
+    ''')
