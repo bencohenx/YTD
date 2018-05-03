28fda3d2f95fba211ad799eb3b0b64e8a5d378e3
Ben <Benc10128@gmail.com>
2018-05-03 13:01:38 -0700

    changing argparse

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 import requests
+from ctypes import *
 from bs4 import BeautifulSoup as bs4
 import argparse
 import os.path
-import sys
+import os
 from datetime import datetime
+import concurrent.futures
+from queue import Queue
+import threading
+import msvcrt
+
+
+class _CursorInfo(Structure):
+    _fields_ = [("size", c_int),
+                ("visible", c_byte)]
+
+
+class COORD(Structure):
+    _fields_ = [("x", c_short), ("y", c_short)]
+
 
 parser = argparse.ArgumentParser(description='''
 __   __             _           _                                
@@ -28,6 +43,7 @@ parser.add_argument('-s', "--song-name")
 parser.add_argument('-l', '--list-path')
 parser.add_argument('-o', '--output-path', default="YoutubeDownloaderAudio")
 group.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument('-w', "--worker-threads", default=3, type=int)
 
 
 def save_html_youtube(html):
@@ -46,7 +62,7 @@ def get_url_vid(name):
     search_results = requests.get(youtube_results_url, params={'search_query': name})
     html_doc = search_results.content
     # save_html_youtube(html_doc)
-    soup = bs4(html_doc, 'lxml')
+    soup = bs4(html_doc.decode('utf-8', 'ignore'), "lxml")
     vid_id = soup.findAll("button", {"title": "Watch later"})[0].get("data-video-ids")
     return 'https://www.youtube.com/watch?v=' + vid_id
 
@@ -60,7 +76,7 @@ def get_download_url(yb_url):
     return 'http://www.convertmp3.io' + href_id
 
 
-def save_file(name, d_url):
+def save_file(song_id, name, d_url):
     with open(os.path.join(args.output_path, name) + '.mp3', 'wb') as f:
         response = requests.get(d_url, stream=True)
         if args.quiet:
@@ -73,63 +89,106 @@ def save_file(name, d_url):
                 dl = 0
                 total_length = int(total_length)
                 for data in response.iter_content(chunk_size=4096):
+                    if stop:
+                        return
                     dl += len(data)
                     f.write(data)
-                    done = int(50 * dl / total_length)
-                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
-                    sys.stdout.flush()
-                print('\n')
+                    done = (dl / total_length) * 50
+                    print_queue.put((song_id * 5 + 3, "[%s%s]" % ('=' * int(done), ' ' * (50 - int(done)))))
 
 
-def merger(song_name, song_id_list, list_amount):
+def merger(song_id_list, song_name):
+    if stop:
+        return
     if args.quiet:
-        print('Download: ' + song_name)
+        print('Downloading: ' + song_name)
     else:
-        print(datetime.now().strftime('%H:%M:%S')+" Start download - " + str(song_id_list) + "/" + str(list_amount)
-              + ": "+ song_name)
+        print_queue.put((song_id_list * 5,
+                         datetime.now().strftime('%H:%M:%S') + " Starting download - " + str(
+                             song_id_list + 1) + "/" + str(
+                             amount)
+                         + ": " + song_name))
     youtube_url = get_url_vid(song_name.replace(" ", "+"))
     if args.quiet is False:
-        print('getting the youtube URL: '+youtube_url)
+        print_queue.put((song_id_list * 5 + 1, 'Getting the youtube URL: ' + youtube_url))
     mp3io_to_download = get_download_url(youtube_url)
     if args.quiet is False:
-        print('getting the youtube URL: '+mp3io_to_download)
-    save_file(song_name, mp3io_to_download)
-    songs_dict[song_name] = [youtube_url, mp3io_to_download]
+        print_queue.put((song_id_list * 5 + 2, 'Getting the mp3io URL: ' + mp3io_to_download))
+    save_file(song_id_list, song_name, mp3io_to_download)
+
+
+def printer():
+    h = windll.kernel32.GetStdHandle(-11)
+    ci = _CursorInfo()
+    windll.kernel32.GetConsoleCursorInfo(h, byref(ci))
+    ci.visible = False  # hide cmd cursor
+    windll.kernel32.SetConsoleCursorInfo(h, byref(ci))
+    offset = 3
+    for y, st in iter(print_queue.get, None):
+        windll.kernel32.SetConsoleCursorPosition(h, COORD(0, offset + y))
+        print(st)
+        print_queue.task_done()
+    if args.quiet:
+        windll.kernel32.SetConsoleCursorPosition(h, COORD(0, offset + amount + 1))
+    else:
+        windll.kernel32.SetConsoleCursorPosition(h, COORD(0, offset + amount * 5 + 1))
+    if stop is False:
+        print("YoutubeDownloader done at: " + datetime.now().strftime('%H:%M:%S'))
+    windll.kernel32.GetConsoleCursorInfo(h, byref(ci))
+    ci.visible = True  # visible cmd cursor
+    windll.kernel32.SetConsoleCursorInfo(h, byref(ci))
+
+
+def stop_by_user():
+    global stop
+    if msvcrt.getch() == b'\x03':
+        stop = True
+    print_queue.put((amount*5, "Stopped by user"))
 
 
 youtube_results_url = "https://www.youtube.com/results"
 mp3io_download_url = 'http://www.convertmp3.io/download/'
-songs_dict = {}
 args = parser.parse_args()
+print_queue = Queue()
+amount = 1
+stop = False
 
-print("Running '{}'\n".format(__file__))
+os.system('cls')
+os.system('mode 800')
+print("Running '{}'\n"
+      "Worker threads are: {}".format(__file__, args.worker_threads))
+
+print_thread = threading.Thread(target=printer)
+stop_thread = threading.Thread(target=stop_by_user)
+stop_thread.daemon = True
+print_thread.start()
+stop_thread.start()
 
 if args.output_path is not None:
     if os.path.isdir(args.output_path):
         pass
     else:
         os.makedirs(args.output_path)
-try:
-    if args.song_name is not None:
-        merger(args.song_name, '1', '1')
-    elif args.list_path is not None:
-        if os.path.exists(args.list_path):  # works on songs list
-            with open(args.list_path, "r", encoding="utf8") as f:
-                songs_list = f.read().split('\n')
-            i = 1
-            amount = len(songs_list)
-            for song in songs_list:
-                merger(song, i, amount)
-                i += 1
-        else:
-            print('''
-                  *** Wrong path list.. ***
-                  ''')
+
+
+if args.song_name is not None:
+    merger(0, args.song_name)
+elif args.list_path is not None:
+    if os.path.exists(args.list_path):  # works on songs list
+        with open(args.list_path, "r", encoding="utf8") as f:
+            songs_list = set(f.read().split('\n'))  # unique list
+        amount = len(songs_list)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=args.worker_threads) as executor:
+            executor.map(merger, range(0, amount), songs_list)
     else:
         print('''
-        *****************************************
-        ***  Bad Args, try to run --help   ***
-        *****************************************
-        ''')
-except KeyboardInterrupt:
-    print("\nStopped by user")
+              *** Wrong path list.. ***
+              ''')
+else:
+    print('''
+    *****************************************
+    ***  Bad Args, try to run --help   ***
+    *****************************************
+    ''')
+
+print_queue.put(None)
